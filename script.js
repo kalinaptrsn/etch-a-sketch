730564881aec948a6c33cdce421b4188cca74b7c
Kalina Petersen <petersenkalina@gmail.com>
2023-01-24 11:22:20 -0800

    Add erase and rainbow functionality

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const container = document.querySelector('.container');
 const rainbow = document.querySelector('#rainbow');
 const erase = document.querySelector('#erase');
+let color = 'black';
+
+const getRainbowColor = () => {
+    const randomNum1 = Math.random() * 255;
+    const randomNum2 = Math.random() * 255;
+    const randomNum3 = Math.random() * 255;
+    return `rgb(${randomNum1}, ${randomNum2}, ${randomNum3})`;
+};
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement('div');
-    div.style.cssText = "border: 1px solid black; height: 25px; width: 25px"; 
+    div.style = "border: 1px solid black; height: 25px; width: 25px"; 
     container.appendChild(div);
     div.addEventListener('mouseover', function(e) {
-        this.style.backgroundColor = 'black';
+        const hoverColor = color === 'rainbow' 
+            ? getRainbowColor() 
+            : color;
+        this.style.backgroundColor = hoverColor;
     });
   }
 
-  
+  erase.addEventListener('click', () => {
+        color = 'white';
+  });
+
+  rainbow.addEventListener('click', () => {
+        color = 'rainbow';
+  })
+
+
+
